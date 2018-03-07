00c5c171064fde0af0d3c5d05087094bad71dd50
LYH <250193425@qq.com>
2018-03-07 17:37:14 +0800

    no message

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,15 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.dao.UserMapper;
 import com.example.demo.entity.User;
 import com.github.pagehelper.PageHelper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class TestController {
